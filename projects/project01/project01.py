f1c8ee80f407b03aa6dea9a933e5a422d31f7560
wenger-margaret <wenger.ma@northeastern.edu>
2026-09-10 17:52:14 -0400

    commiting initial function setups and pseudocode

--- projects/project01/project01.py
+++ projects/project01/project01.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python
+from pprint import pprint
 
+
+# Modify this function signature and fill in the details
+def parse_line(line: str)
+    pass
+
+
+# Modify this function signature and fill in the details
+def read_file(file: str):
+    with open(file) as f:
+        for line in f:
+            if (line.startswith("#")): continue  # filters out metadata lines
+            #print(line)
+            # pass to parse_line to extract needed data
+            parse_line(line)
+
+
+
+if __name__ == "__main__":
+    pprint(read_file("clinvar_20190923_short.vcf"))
