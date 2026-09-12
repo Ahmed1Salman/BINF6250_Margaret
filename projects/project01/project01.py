6795742f874ed1e036beb6cb3227c4ffab15d6a2
wenger-margaret <wenger.ma@northeastern.edu>
2026-09-12 16:52:05 -0400

    working on extracting info from lines

--- projects/project01/project01.py
+++ projects/project01/project01.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python
 from pprint import pprint
+from symbol import continue_stmt
 
 
 # Modify this function signature and fill in the details
-def parse_line(line: str)
-    pass
+def parse_line(line: str):
+    # splits line by tabs, takes last item (all the info starting with ALLELEID)
+    new_line = line.rstrip().split('\t')[-1]
+    # split new_line by ;, list
+    gene_info = new_line.split(';')
+
+    # TODO: how to find AF_EXAC (not is same place in each list)
+    if "AF_EXAC" in gene_info:
+        pos = gene_info.index('AF_EXAC')
+        AFC_EXAC = gene_info[pos]
+        print(AFC_EXAC)
+    # TODO: find CLNDN
+    #CLNDN = gene_info.find('CLNDN')
+    #if CLNDN != -1:
+
+    # initialize empty list to add rare genes to
+    #rare_genes = []
+    #if AF_EXAC < 0.0001:
+     #   if CLNDN == "not_specified" or CLNDN == "not_provided":
+      #      continue
+      #  else:
+      #      rare_genes.append(CLNDN)
+
+    print(gene_info)
 
 
 # Modify this function signature and fill in the details
